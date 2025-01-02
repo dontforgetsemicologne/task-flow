8401e5f72fd5b750f83b589df02e1505c6e20b57
Alen Cyriac Joseph <semicologne13@outlook.com>
2025-01-02 22:01:55 +0530

    chore: init Next.js with tRPC and Prisma setup
    Set up Next.js project with tRPC for type-safe APIs and Prisma for database management. Includes proper configuration and initial setup.

---
+++ components/TRPCProvider.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useState } from "react"
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { httpBatchLink } from "@trpc/client";
+
+import { trpc } from "@/server/client";
+
+export default function TRPCProvider({ children }: { children: React.ReactNode}) {
+
+    const [ queryClient ] = useState(() => new QueryClient({}));
+    const [ trpcClient ] = useState(
+        trpc.createClient({
+            links: [
+                httpBatchLink({
+                    url: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+                }),
+            ],
+        })
+    );
+
+    return(
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        </trpc.Provider>
+    )
+}
---
+++ server/index.ts
@@ -0,0 +1,7 @@
+import { procedure, router } from '@/server/trpc'
+
+export const appRouter = router({
+    
+});
+
+export type AppRouter = typeof appRouter;
